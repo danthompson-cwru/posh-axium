45c5c2340799819dbb8eb611491832cd14575df0
Daniel Thompson <danthompson@case.edu>
2021-01-21 16:15:24 -0500

    Added Get-MSIProperties
    Needed for #3.

---
+++ Axium/1.2.0/Functions/Get-MSIProperties.ps1
@@ -0,0 +1,118 @@
+function Get-MSIProperties {
+    <#
+        .SYNOPSIS
+            Gets the properties of an MSI.
+
+        .DESCRIPTION
+            Gets the properties of an MSI.
+
+            Aliases: gmp
+
+        .INPUTS
+            System.IO.FileInfo
+
+        .OUTPUTS
+            hashtable
+
+        .NOTES
+            Author    : Dan Thompson
+            Copyright : 2020 Case Western Reserve University
+    #>
+
+    [CmdletBinding()]
+    [OutputType([hashtable])]
+
+    param(
+        # The path to the MSI.
+        #
+        # Aliases: p
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True,
+            Mandatory = $True
+        )]
+        [Alias('p')]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({ $_ | Test-Path -PathType 'Leaf' })]
+        [System.IO.FileInfo]$Path
+    )
+
+    begin {
+        $Com = New-Object -Com 'WindowsInstaller.Installer'
+    }
+
+    process {
+        $Properties = @{}
+
+        try {
+            $Database = $Com.GetType().InvokeMember(
+                'OpenDatabase',
+                'InvokeMethod',
+                $Null,
+                $Com,
+                @($Path.FullName, 0)
+            )
+
+            $Query = 'SELECT * FROM Property'
+
+            $View = $Database.GetType().InvokeMember(
+                'OpenView',
+                'InvokeMethod',
+                $Null,
+                $Database,
+                ($Query)
+            )
+
+            $View.GetType().InvokeMember(
+                'Execute',
+                'InvokeMethod',
+                $Null,
+                $View,
+                $Null
+            )
+
+            $Record = $View.GetType().InvokeMember(
+                'Fetch',
+                'InvokeMethod',
+                $Null,
+                $View,
+                $Null
+            )
+
+            while ($Null -ne $Record) {
+                $PropertyName = $Record.GetType().InvokeMember(
+                    'StringData',
+                    'GetProperty',
+                    $Null,
+                    $Record,
+                    1
+                )
+
+                $PropertyValue = $Record.GetType().InvokeMember(
+                    'StringData',
+                    'GetProperty',
+                    $Null,
+                    $Record,
+                    2
+                )
+
+                $Properties[$PropertyName] = $PropertyValue
+
+                $Record = $View.GetType().InvokeMember(
+                    'Fetch',
+                    'InvokeMethod',
+                    $Null,
+                    $View,
+                    $Null
+                )
+            }
+        } catch {
+            throw "Failed to get the properties of the MSI at $($Path.FullName). The error was: $_"
+        }
+
+        $Properties
+    }
+}
+
+New-Alias -Name 'gmp' -Value 'Get-MSIProperties'
